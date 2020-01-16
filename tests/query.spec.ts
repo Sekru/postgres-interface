65f791263aababff8b59e3e468b1a2a1178b1606
Sekru <rusekpawel1@gmail.com>
2020-01-16 20:55:07 +0100

    Add files via upload

--- tests/query.spec.ts
+++ tests/query.spec.ts
@@ -7,81 +7,84 @@ describe('Query builder tests', () => {
       const r = new Query({});
       r.table('test');
 
-      expect(r.query.join(' ')).equal('SELECT * FROM test');
+      expect(r.toString()).equal('SELECT * FROM test');
     });
 
     it('should build select with conditions', () => {
       const r = new Query({})
       r.table('test').filter({'id': 1});
 
-      expect(r.query.join(' ')).equal('SELECT * FROM test WHERE id = $1');
-      expect(r.params[0]).equal(1);
+      expect(r.toString()).equal('SELECT * FROM test WHERE id = 1');
     });
 
     it('should build select with conditions and order desc', () => {
       const r = new Query({});
       r.table('test').filter({'id': 1}).orderBy(r.desc('id'));
 
-      expect(r.query.join(' ')).equal('SELECT * FROM test WHERE id = $1 ORDER BY id DESC');
-      expect(r.params[0]).equal(1);
+      expect(r.toString()).equal('SELECT * FROM test WHERE id = 1 ORDER BY id DESC');
     });
 
     it('should build select by function', () => {
       const r = new Query({});
-      r.table('test').filter(r.row('age').gt(18));
+      r.table('test').filter(r.row('age').gt(18).run());
 
-      expect(r.query.join(' ')).equal('SELECT * FROM test WHERE age > 18');
+      expect(r.toString()).equal('SELECT * FROM test WHERE age > 18');
     });
 
     it('should build between', () => {
       const r = new Query({});
       r.table('test').between(200, 300, 'field');
 
-      expect(r.query.join(' ')).equal('SELECT * FROM test WHERE field BETWEEN 200 AND 300');
-    });
-
-    it('should build custom', () => {
-      const r = new Query({});
-      r.custom('SELECT 1');
-
-      expect(r.query.join(' ')).equal('SELECT 1');
+      expect(r.toString()).equal('SELECT * FROM test WHERE field BETWEEN 200 AND 300');
     });
 
     it('should build insert', () => {
       const r = new Query({});
       r.table('test').insert({'id': 1});
 
-      expect(r.query.join(' ')).equal('INSERT INTO test(id) VALUES($1)');
-      expect(r.params[0]).equal(1);
+      expect(r.toString()).equal('INSERT INTO test(id) VALUES(1)');
     });
 
     it('should build update', () => {
       const r = new Query({});
       r.table('test').update({'id': 1, 'name': 'test'});
 
-      expect(r.query.join(' ')).equal('UPDATE test SET id = 1, name = test');
+      expect(r.toString()).equal('UPDATE test SET id = 1, name = test');
     });
 
     it('should build filter and update', () => {
       const r = new Query({});
       r.table('test').filter({'name': 'test'}).update({'name': 'test2', 'foo': 'bar'});
 
-      expect(r.query.join(' ')).equal('UPDATE test SET name = test2, foo = bar WHERE name = $1');
-      expect(r.params[0]).equal('test');
+      expect(r.toString()).equal('UPDATE test SET name = test2, foo = bar WHERE name = test');
     });
 
     it('should build filter and delete', () => {
       const r = new Query({});
       r.table('test').filter({'name': 'test'}).delete();
 
-      expect(r.query.join(' ')).equal('DELETE FROM test WHERE name = $1');
-      expect(r.params[0]).equal('test');
+      expect(r.toString()).equal('DELETE FROM test WHERE name = test');
     });
 
     it('should build hasFields', () => {
       const r = new Query({});
       r.table('test').hasFields('foo', 'bar');
 
-      expect(r.query.join(' ')).equal('SELECT * FROM test WHERE foo IS NOT NULL AND bar IS NOT NULL');
+      expect(r.toString()).equal('SELECT * FROM test WHERE foo IS NOT NULL AND bar IS NOT NULL');
+    });
+
+    it('should build extreme query', () => {
+      const r = new Query({});
+        r.table('test')
+          .hasFields('id')
+          .filter({'foo': 'bar'})
+          .filter((row: any) => {
+              return row('valueStart')
+                  .during(10, 20, { rightBound: 'closed' })
+                  .or(row('valueEnd').during(10, 20, { rightBound: 'closed' }).toString())
+                  .or(row('valueStart').lt(10).and(row('valueEnd').gt(20).toString()).toString()).toString();
+          });
+
+        expect(r.toString()).equal(`SELECT * FROM test WHERE id IS NOT NULL AND foo = bar AND valueStart <= 10 AND valueStart >= 20 AND (valueEnd <= 10 AND valueEnd >= 20 ) OR (valueStart < 10 AND (valueEnd > 20 ) )`);
     });
 });
